4bb63469aa4aa87277dfa8910dedeee8e840adf8
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-01-22 10:14:06 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Perkins, Alexander (ABH) on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
